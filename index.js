b72ea50edcb63f9dcfb2cfbbc3f4ce4b2f4fa05c
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-12-14 17:30:06 -0500

    work on adding multer

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const cors = require('cors');
 require('dotenv').config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+//Post uploaded file
+app.post('/api/fileanalyse', upload.single('file'), (req, res) => {
+console.log(req);
 
-
+  res.json({
+    name: req.body.upfile,
+    type: "",
+    size: ""
+  })
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
