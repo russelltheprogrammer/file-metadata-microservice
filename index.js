315bff92e4db79166bc849e50b1152048633323d
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-12-15 14:46:03 -0500

    make changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'upload/' });
 const cors = require('cors');
 require('dotenv').config();
 
@@ -8,21 +8,18 @@ const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 //Post uploaded file
-app.post('/api/fileanalyse', upload.single('file'), (req, res) => {
-console.log(req);
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
   res.json({
-    name: req.body.upfile,
-    type: "",
-    size: ""
-  })
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
 
 const port = process.env.PORT || 3001;
